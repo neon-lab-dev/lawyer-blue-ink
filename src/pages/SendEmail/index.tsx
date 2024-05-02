95c44a9f2e84e7afe19162b5b2e22755436593ca
Tilak Thapa <iamjrtilak@gmail.com>
2024-05-02 08:16:36 +0545

    Fix Excel file details state initialization in SendEmail component

--- src/pages/SendEmail/index.tsx
+++ src/pages/SendEmail/index.tsx
@@ -7,6 +7,7 @@ import ExcelSheet from "./ExcelSheet";
 const SendEmail = () => {
   const [selectedPage, setSelectedPage] = useState("select-template");
   const [selectedTemplate, setSelectedTemplate] = useState<string>("");
+  //@ts-ignore
   const [excelFileDetails, setExcelFileDetails] = useState<any>({});
 
   if (selectedPage === "select-template") {
